ca6b39091824d1100e1366b873339e04c6b772c3
song_li <song_li@realsil.com.cn>
2018-05-05 18:31:08 +0800

    remove count page

--- pages/game/game.js
+++ pages/game/game.js
@@ -5,69 +5,70 @@ Page({
    * 页面的初始数据
    */
   data: {
-    array: [{
-      idd: 1,
-      message: 'foo',
-    }, {
-      idd: 2,
-      message: 'bar'
-    }]
+    gms: [
+      {das:[1, 5, 5, 5]},
+      {das:[2, 2, 7, 7]},
+      {das:[2, 4, 10, 10]},
+      {das:[3, 3, 8, 8]},
+      {das:[4, 4, 7, 7]},
+      {das:[4, 4, 10, 10]},
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
   goPlay: function(event){
